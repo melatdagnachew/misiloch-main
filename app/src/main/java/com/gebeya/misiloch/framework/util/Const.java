7ccb2e794ff3ac21e666acd980a677d161be273a
saladthieves <muhiregide@gmail.com>
2019-10-10 19:43:09 +0300

    Initial commit

---
+++ app/src/main/java/com/gebeya/misiloch/framework/util/Const.java
@@ -0,0 +1,6 @@
+package com.gebeya.misiloch.framework.util;
+
+public final class Const {
+    public static final String PREFS_NAME = "MISILOCH_PREFS";
+    public static final String DATABASE_NAME = "misiloch_db";
+}
---
+++ app/src/main/java/com/gebeya/misiloch/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.gebeya.misiloch.splash;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.gebeya.misiloch.framework.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
