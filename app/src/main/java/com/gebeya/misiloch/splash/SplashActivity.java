5b04c1ddcd7c4e22751669dff04a53cc7c7481be
saladthieves <muhiregide@gmail.com>
2019-10-11 21:26:24 +0300

    Fix startup issues

--- app/src/main/java/com/gebeya/misiloch/splash/SplashActivity.java
+++ app/src/main/java/com/gebeya/misiloch/splash/SplashActivity.java
@@ -1,14 +1,11 @@
 package com.gebeya.misiloch.splash;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import com.gebeya.misiloch.App;
 import com.gebeya.misiloch.R;
 import com.gebeya.misiloch.framework.base.BaseActivity;
-import com.gebeya.misiloch.welcome.WelcomeActivity;
 
 public class SplashActivity extends BaseActivity {
 
@@ -21,9 +18,5 @@ public class SplashActivity extends BaseActivity {
                 .beginTransaction()
                 .add(R.id.splashFragmentContainer, new SplashFragment())
                 .commit();
-
-        App app = (App) getApplication();
-        app.setName("Hello world!");
-        startActivity(new Intent(this, WelcomeActivity.class));
     }
 }
