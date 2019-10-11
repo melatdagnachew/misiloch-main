9136a244187ff9ff49affa71f7b4b9d81e39ab55
saladthieves <muhiregide@gmail.com>
2019-10-11 18:28:36 +0300

    Add welcome activity

---
+++ app/src/main/java/com/gebeya/misiloch/welcome/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.gebeya.misiloch.welcome;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.gebeya.misiloch.R;
+import com.gebeya.misiloch.framework.base.BaseActivity;
+
+public class WelcomeActivity extends BaseActivity  {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
