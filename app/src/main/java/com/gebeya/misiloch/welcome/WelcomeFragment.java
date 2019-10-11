521623210e9fd70dac16f2ac0bee74ce50f303f5
saladthieves <muhiregide@gmail.com>
2019-10-11 21:08:02 +0300

    Add missing WelcomeFragment implementation

--- app/src/main/java/com/gebeya/misiloch/welcome/WelcomeFragment.java
+++ app/src/main/java/com/gebeya/misiloch/welcome/WelcomeFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.gebeya.misiloch.R;
 import com.gebeya.misiloch.framework.base.BaseFragment;
 
 public class WelcomeFragment extends BaseFragment {
@@ -15,7 +16,7 @@ public class WelcomeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        inflate(R.layout.fragment_welcome, container);
 
         return root;
     }
