1e65da73b839716913d9c12e07ca43bf373bcd81
saladthieves <muhiregide@gmail.com>
2019-10-11 20:42:46 +0300

    Add fragment preparation and content

---
+++ app/src/main/java/com/gebeya/misiloch/welcome/WelcomeFragment.java
@@ -0,0 +1,22 @@
+package com.gebeya.misiloch.welcome;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.gebeya.misiloch.framework.base.BaseFragment;
+
+public class WelcomeFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return root;
+    }
+}
