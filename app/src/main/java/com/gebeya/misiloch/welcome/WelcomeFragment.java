ce5faa1dd0bf5effd7bdfa5b88f320d01466cd76
saladthieves <muhiregide@gmail.com>
2019-10-12 09:11:26 +0300

    Fix missing fragment id

--- app/src/main/java/com/gebeya/misiloch/welcome/WelcomeFragment.java
+++ app/src/main/java/com/gebeya/misiloch/welcome/WelcomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +13,18 @@ import androidx.annotation.Nullable;
 import com.gebeya.misiloch.R;
 import com.gebeya.misiloch.framework.base.BaseFragment;
 
+import butterknife.BindView;
+
 public class WelcomeFragment extends BaseFragment {
+
+    @BindView(R.id.welcomeImage)
+    ImageView image;
+
+    @BindView(R.id.welcomeTitle)
+    TextView title;
+
+    @BindView(R.id.welcomeDescription)
+    TextView description;
 
     @Nullable
     @Override
