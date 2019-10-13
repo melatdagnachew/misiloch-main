0febcc0f25d1a2d6d431f7a6bf0fe61aa85d7d86
saladthieves <muhiregide@gmail.com>
2019-10-13 19:21:50 +0300

    Add version info

--- app/src/main/java/com/gebeya/misiloch/splash/SplashFragment.java
+++ app/src/main/java/com/gebeya/misiloch/splash/SplashFragment.java
@@ -4,23 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
+import com.gebeya.misiloch.BuildConfig;
 import com.gebeya.misiloch.R;
 import com.gebeya.misiloch.framework.base.BaseFragment;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class SplashFragment extends BaseFragment {
+
+    @BindView(R.id.splashVersion)
+    TextView versionLabel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         inflate(R.layout.fragment_splash, container);
+
+        versionLabel.setText(BuildConfig.VERSION_NAME);
 
         return root;
     }
