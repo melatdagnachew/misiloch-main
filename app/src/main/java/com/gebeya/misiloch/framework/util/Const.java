e8131b2ffb783ebab2651d279acf5439350e97ae
saladthieves <muhiregide@gmail.com>
2019-10-14 21:07:02 +0300

    Add base URL in const

--- app/src/main/java/com/gebeya/misiloch/framework/util/Const.java
+++ app/src/main/java/com/gebeya/misiloch/framework/util/Const.java
@@ -3,4 +3,6 @@ package com.gebeya.misiloch.framework.util;
 public final class Const {
     public static final String PREFS_NAME = "MISILOCH_PREFS";
     public static final String DATABASE_NAME = "misiloch_db";
+
+    public static final String BASE_URL = "https://misiloch.herokuapp.com/api/v1/";
 }
